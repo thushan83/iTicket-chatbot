576214d9e5a2758698fd609db40f53eb8ee93db7
Nathalie Larsson <nathalie.larsson@visit.com>
2023-11-23 13:41:21 +0100

    API call fix

--- clippy/src/API/index.js
+++ clippy/src/API/index.js
@@ -3,7 +3,10 @@ import axios from 'axios'
 export async function askAIQuesion(payload){
     try {
     console.log('api payload', payload)
-        const response = await axios.get('http://127.0.0.1:5000/chat', payload)
+    const data = {
+        question: payload
+    }
+        const response = await axios.post('http://127.0.0.1:5000/chat', data)
     console.log('api response LOOKUP', response.data)
     return response.data
 } catch (error) {
