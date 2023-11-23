4648c5dab12f7f13e012ea363ebb55c28652b53e
Nathalie Larsson <nathalie.larsson@visit.com>
2023-11-23 10:05:47 +0100

    input added

---
+++ clippy/src/components/AskQuestion.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+ <input v-model="question" type="text" placeholder="Ask your question">
+  <button @click="ask()">Ask</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'AskQuestion',
+  data () {
+    return {
+      question: ''
+    }
+  },
+  methods: {
+  ask () {
+    console.log(this.question)
+  }
+}
+}
+</script>
+<style>
+input{
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+}
+button{
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  background: #eee;
+}
+
+</style>
