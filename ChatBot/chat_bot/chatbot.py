6f7c3e8e49ccf475068734580774261e99e6b4c3
Thushan Gunawardana <thushan.gunawardana@visit.com>
2023-11-23 11:41:54 +0100

    Added REST end point /chat to handle questions and send back answer

--- ChatBot/chat_bot/chatbot.py
+++ ChatBot/chat_bot/chatbot.py
@@ -1,6 +1,26 @@
+import json
+from flask import Flask,request, json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello from iTicket chat server"
+
+
+@app.route('/chat', methods=['POST'])
+def chatlistner():
+    return chat_response(request)
+
 
 chatbot_name = "iTicket: "
 guest = "You: "
+
+def chat_response(request):
+    question = request.json.get("question")
+    default_response = "Sorry I do not have the answer, or I do not understand your question"
+    return json.dumps({'res': ''+default_response+'','question': ''+question+''})
+
 
 def main():
     default_response = "Sorry I do not have the answer, or I do not understand your question"
@@ -10,6 +30,7 @@ def main():
         question = input(guest)
         print(chatbot_name, default_response)
 
+app.run()
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    main()
