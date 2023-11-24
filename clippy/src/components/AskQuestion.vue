32280ee58bbfe0379c6231091fbcb6d2a8f1e597
Nathalie Larsson <nathalie.larsson@visit.com>
2023-11-24 10:30:45 +0100

    prettier

--- clippy/src/components/AskQuestion.vue
+++ clippy/src/components/AskQuestion.vue
@@ -1,17 +1,23 @@
 <template>
-  <div class="all-qestion-container">
-    <div id="scroller"  v-for="(askedQuestion, i) in AllQuestions" :key="askedQuestion" >
-      <p class="asked-question">{{ askedQuestion }}</p>
-      <p v-for=" (answer, index) in answers" :key="answer">
-        <span v-if="index == i">
-          {{ answer }}
-        </span>
-      </p>
+  <div class="container">
+    <div class="all-qestion-container">
+      <div id="scroller"  v-for="(askedQuestion, i) in AllQuestions" :key="askedQuestion" >
+        <p class="asked-question">
+          <span class="test">
+            Me: {{ askedQuestion }}
+          </span>
+        </p>
+        <p v-for="(answer, index) in answers" :key="answer">
+          <span v-if="index == i">
+            ITicket: {{ answer }}
+          </span>
+        </p>
+      </div>
     </div>
-  </div>
-  <div>
- <input v-model="question" type="text" placeholder="Ask your question">
-  <button @click="ask()">Ask</button>
+    <div>
+      <input v-model="question" type="text" placeholder="Ask your question">
+      <button @click="ask()">Ask</button>
+    </div>
   </div>
 </template>
 <script>
@@ -44,6 +50,11 @@ export default {
 }
 </script>
 <style>
+.container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 input{
   padding: 0.5rem;
   border: 1px solid #ccc;
@@ -57,10 +68,19 @@ button{
   border: 1px solid #ccc;
   max-height: 10rem;
   overflow: scroll;
+  display: contents;
+  width: 30rem;
 }
 .asked-question{
   padding: 0.5rem;
   margin-bottom: 0.5rem;
+}
+.test{
+  width: fit-content;
+  background-color: beige;
+  padding: 0 0.5rem;
+  float: inline-end;
+
 }
 
 </style>
